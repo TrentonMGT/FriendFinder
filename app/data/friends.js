67fc2622d6e3e9abf7adf6be59dc37afe0439c9e
Trenton Mcleod <Boanerges@Trentons-MBP.attlocal.net>
2017-04-30 02:40:37 -0400

    testing routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,93 @@
-var friendsArray = [
-	{
-		name: 'FooBar',
-		photo: 'ahmed@example.com',
-		scores: []
-	}
-];
-
+var friendsArray = [{
+  'name': "The Rock",
+  'photo': "https://en.wikipedia.org/wiki/Dwayne_Johnson#/media/File:Dwayne_Johnson_2,_2013.jpg",
+  'scores': [
+      "5",
+      "2",
+      "5",
+      "1",
+      "4",
+      "4",
+      "3",
+      "2",
+      "1",
+      "1"
+  ]
+}, {
+  'name': "The Ultimate Warrior",
+  'photo': "https://en.wikipedia.org/wiki/The_Ultimate_Warrior#/media/File:Randy_Savage_vs_Ultimate_Warrior.jpg",
+  'scores': [
+      "3",
+      "1",
+      "5",
+      "3",
+      "2",
+      "4",
+      "3",
+      "1",
+      "2",
+      "4"
+  ]
+}, {
+  'name': "Stone Cold",
+  'photo': "https://en.wikipedia.org/wiki/Stone_Cold_Steve_Austin#/media/File:Stone_Cold_Steve_Austin_in_1996.jpg",
+  'scores': [
+      "5",
+      "2",
+      "4",
+      "1",
+      "1",
+      "2",
+      "3",
+      "5",
+      "1",
+      "4"
+  ]
+}, {
+  'name': "Hulk Hogan",
+  'photo': "https://en.wikipedia.org/wiki/Hulk_Hogan#/media/File:Hulk_Hogan.jpg",
+  'scores': [
+      "4",
+      "1",
+      "3",
+      "1",
+      "5",
+      "4",
+      "2",
+      "2",
+      "5",
+      "1"
+  ]
+}, {
+  'name': "HHH",
+  'photo': "https://en.wikipedia.org/wiki/Triple_H#/media/File:Triple_H_in_April_2016.jpg",
+  'scores': [
+      "3",
+      "2",
+      "3",
+      "3",
+      "4",
+      "5",
+      "2",
+      "3",
+      "4",
+      "5"
+  ]
+}, {
+  'name': "Rey Mysterio",
+  'photo': "https://en.wikipedia.org/wiki/Rey_Mysterio#/media/File:Rey_Mysterio_October_2015.jpg",
+  'scores': [
+      "2",
+      "3",
+      "1",
+      "5",
+      "5",
+      "4",
+      "2",
+      "1",
+      "2",
+      "5"
+  ]
+}];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,58 +1,61 @@
-var userData = require('../data/friends.js');
+var friendsData = require('../data/friends.js');
 
-//app represents express which was named in the server.js file
+var path = require('path');
+
+// ============================
+// ROUTING
+// ============================
+
 module.exports = function(app) {
 
-    //get and diplay the table data in json format 
     app.get('/api/friends', function(req, res) {
-        res.json(userData);
+        res.json(friendsData);
     });
 
-        //get and diplay the table data in json format 
+
     app.post('/api/friends', function(req, res) {
-            userData.push(req.body);
-        
+
+
+        var bestMatch = 0;
+        var bestDiff = 1000;
+
+
+        for (var i = friendsData.length - 1; i >= 0; i--) {
+
+            console.log("comparing with " + friendsData[i].name);
+
+            var totalDifference = 0;
+
+
+            for (var k = 0; k < friendsData.length; k++) {
+
+                //console.log("someone did " + friendsData[i].scores[k]);
+                //console.log("you entered " +  req.body.scores[k]);
+                //console.log("you differed by " + Math.abs(friendsData[i].scores[k] - req.body.scores[k]) );
+
+                totalDifference = totalDifference + Math.abs(friendsData[i].scores[k] - req.body.scores[k]);
+
+            }
+
+            if (totalDifference < bestDiff) {
+                bestDiff = totalDifference;
+                bestMatch = i;
+            }
+
+            console.log("total difference for " + friendsData[i].name + " is " + totalDifference);
+
+        }
+
+        console.log("=============================");
+        console.log("best person is " + friendsData[bestMatch].name + " and best score is " + bestDiff);
+        console.log("=============================");
+
+        // push in the user input into the friendArray
+        friendsData.push(req.body);
+
+        // respond back with the best match
+        res.json({ name: friendsData[bestMatch].name, photo: friendsData[bestMatch].photo }); // KEY LINE
+
     });
-
-  // app.post('/api/friend', function(req, res) {
-
-  //     var greatMatch = {
-  //         name: "",
-  //         image: "",
-  //         matchDifference: 1000
-  //     };
-  //     var usrData = req.body;
-  //     var usrName = usrData.name;
-  //     var usrImage = usrData.image;
-  //     var usrScores = usrData.scores;
-
-  //     var totalDifference = 0;
-
-  //     //loop through the userData data array of objects to get each userData scores
-  //     for (var i = 0; i < [userData].length - 1; i++) {
-  //         console.log(userData[i].name);
-  //         totalDifference = 0;
-
-  //         //loop through that userData score and the users score and calculate the 
-  //         // absolute difference between the two and push that to the total difference variable set above
-  //         for (var j = 0; j < 10; j++) {
-  //             // We calculate the difference between the scores and sum them into the totalDifference
-  //             totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(userData[i].scores[j]));
-  //             // If the sum of differences is less then the differences of the current "best match"
-  //             if (totalDifference <= greatMatch.friendDifference) {
-
-  //                 // Reset the bestMatch to be the new friend. 
-  //                 greatMatch.name = userData[i].name;
-  //                 greatMatch.photo = userData[i].photo;
-  //                 greatMatch.matchDifference = totalDifference;
-  //             }
-  //         }
-  //     }
-
-  //     userData.push(req.body);
-
-  //     res.json(greatMatch);
-  // });
-
 
 }
